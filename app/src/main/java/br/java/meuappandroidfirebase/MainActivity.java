942e7d7999c7a53b59365df2aa54c48bb1533903
Diego <diegoteixeirapereira@hotmail.com>
2021-11-12 11:35:24 -0300

    Criação do app Android Firebase CRUD

---
+++ app/src/main/java/br/java/meuappandroidfirebase/MainActivity.java
@@ -0,0 +1,24 @@
+package br.java.meuappandroidfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnEntrarMain_Click(View v) {
+
+    }
+
+    public void btnCadastrarMain_Click(View v) {
+
+    }
+
+}
